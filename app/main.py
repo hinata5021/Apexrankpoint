a42681646cd04a0b0a42ceb8a20996228587858e
hinata5021 <pucky1205@icloud.com>
2022-01-16 17:24:50 +0900

    main

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from pprint import pprint
 f = open('player.txt', 'r', encoding='UTF-8')
 datalist = f.readlines()
 Platform = datalist[0]
-PlayerIS = datalist[1]
+PlayerID = datalist[1]
 url = "https://public-api.tracker.gg/v2/apex/standard/profile/" + Platform + "/" + PlayerID + "/sessions"
 f.close()
 
-header = {"TRN-Api-Key":os.environ['TRN_API_KEY']}#環境変数から読み込むようにしてる
+header = {"TRN-Api-Key":os.environ['112293c5-eb03-4ea1-a453-f0419f8e785d']}#環境変数から読み込むようにしてる
 res = open(url, headers=header).json()
 pprint(res)
